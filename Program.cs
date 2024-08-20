f63caff1352683c4a19c4cdb074acb7dd8e11ee7
inzeshtahir <inzesh14@gmail.com>
2024-08-20 19:27:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Registrar.Components;
+using Microsoft.Extensions.Configuration;
+using Registrar;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var connectionString = builder.Configuration.GetConnectionString("default");
+builder.Services.AddSingleton(new Database(connectionString!));
+builder.Services.AddScoped<StudentDataAccess>();
+builder.Services.AddScoped<CourseDataAccess>();
+builder.Services.AddScoped<RegistrationDataAccess>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
